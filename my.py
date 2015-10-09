83c1628284d923f09b678aff3e4789392307e79d
server.france <fahimi72@gmail.com>
2015-10-09 09:35:46 -0400

    some changes

--- my.py
+++ my.py
@@ -55,7 +55,7 @@ def request_data(media_id):
 			faces.append(api.detection.landmark(img=File(IMAGE_PATH + media_id + '.jpg'), face_id=face['face_id']))
 		pickle.dump(res , open(RESULT_PATH + media_id , 'w'))
 		pickle.dump(faces , open(LANDMARK_PATH + media_id , 'w'))
-		logging.info(media_id + ',stored.')
+		#logging.info(media_id + ',stored.')
 	logging.info(media_id + ',failed.')
 
 
@@ -67,20 +67,20 @@ counter = 0
 for media in media_list:
 	try:
 		counter+=1
-		if counter<40:
-			continue
+#		if counter<40:
+#			continue
 		if len(th_list) == 8:
 			for th in th_list:
 				th.join()
 			th_list = list()
 		th = threading.Thread(target=request_data , args=[media])
+		th_list.append(th)
 		th.start()
 		if counter %20 == 0:
 			time.sleep(1)
 		print counter
 	except APIError,x:
-		f = open('error' , 'a')
-		f.write(media,x.code)
+		logging.info(','+media_id + ',' + x.code)
 
 		
 	
