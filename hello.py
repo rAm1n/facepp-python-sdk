b1e5f610711f4a540ae53e5d1f6c673b44780392
Ramin Fahimi <fahimi72@gmail.com>
2015-10-09 12:06:14 +0330

    adding my test script

--- hello.py
+++ hello.py
@@ -9,8 +9,8 @@
 
 # You need to register your App first, and enter you API key/secret.
 # 您需要先注册一个App，并将得到的API key和API secret写在这里。
-API_KEY = '<your API key here>'
-API_SECRET = '<your API secret here>'
+API_KEY = 'd3f3b68bde35104d8494fa647c9498a4'
+API_SECRET = 'lSpWxUz3eVktz-mVITM268Xwhufu7NbJ'
 
 # Import system libraries and define helper functions
 # 导入系统库并定义辅助函数
---
+++ my.py
@@ -0,0 +1,39 @@
+from facepp import *
+import threading
+import time
+from qr import *
+
+IMAGE_PATH = '/home/ramin/'
+RESULT_PATH = '/home/ramin/'
+
+accounts = list()
+
+def init():
+	global accounts
+	q = Queue('facepp')
+	f = open('facepp.cfg' , 'r')
+	for line in f:
+		accounts.append(tuple(line.strip().split(',')))
+		q.push(tuple(line.strip().split(',')))
+	f.close()
+
+
+
+def get_access_token():
+	pass
+
+def request_data(media_id):
+	global IMAGE_PATH,RESULT_PATH		
+	FILE = File(IMAGE_PATH + media_id + '.jpg')
+	token = get_access_token()
+	api = API(token[0],token[1],srv=token[2])
+	print api.detection.detect(img=FILE)
+	print '--------------------'
+	print api.detection.landmark(img=FILE)
+	
+	
+	
+	
+	
+	
+	
